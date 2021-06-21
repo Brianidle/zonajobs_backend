e18d4bac4ddde2f7152cff7f823796876a8fc0cb
Brianidle <ddbrayan10@hotmail.com>
2021-06-21 18:21:42 -0300

    New api path '/jobAd/featuredJobs' created which send the jobs that are classified as featured. At this time this is going to return 8 jobs with a brief description.

--- src/routes/JobAd.js
+++ src/routes/JobAd.js
@@ -23,6 +23,20 @@ jobAdRouter.get("/all", (req, res) => {
   }
 });
 
+jobAdRouter.get("/featuredJobs", (req, res) => {
+  try {
+    JobAdModel.find((err, jobAds) => {
+      jobAds.forEach((jobAd) => {
+        let jobAdDescriptionCutted = jobAd.description.substr(0, 350);
+        jobAd.description = jobAdDescriptionCutted;
+      });
+      res.send(jobAds);
+    }).limit(8);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 jobAdRouter.get("/:id", async (req, res) => {
   try {
     let jobAd = await JobAdModel.findOne({ _id: req.params.id });
